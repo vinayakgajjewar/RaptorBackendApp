f1a739f6082928050b0e9c9e8ad85d60a729b469
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2021-07-18 11:53:54 -0700

    use jackson to serve json content

--- RaptorBackendApp/src/main/java/org/bdlabucr/RaptorServlet.java
+++ RaptorBackendApp/src/main/java/org/bdlabucr/RaptorServlet.java
@@ -2,6 +2,10 @@ package org.bdlabucr;
 
 import java.io.IOException;
 
+// jackson library to read/write json files
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,8 +15,16 @@ public class RaptorServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("text/html");
+        response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>hello world</h1>");
+        //response.getWriter().println("{}");
+        try (JsonGenerator jsonGenerator = new JsonFactory().createGenerator(response.getOutputStream())) {
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeFieldName("field1");
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeNumberField("num1", 3);
+            jsonGenerator.writeEndObject();
+            jsonGenerator.writeEndObject();
+        }
     }
 }
